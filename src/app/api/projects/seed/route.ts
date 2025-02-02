3c7533f27a9445e515758240a17ca73161b5ba16
Sun English <sunenglishjr@gmail.com>
2025-02-02 09:05:38 -0500

    projects updated

--- src/app/api/projects/seed/route.ts
+++ src/app/api/projects/seed/route.ts
@@ -3,6 +3,30 @@ import { NextResponse } from 'next/server';
 
 const initialProjects = [
   {
+    id: 'lcva-portal',
+    name: 'LCVA Portal',
+    description: 'In development is an iOS application for Longwood University/the Longwood Center of Visual Arts. The application, utilizing Swift/SwiftUI and Python/Flask, aims to enhance education, engagement, and business analytics across the campus. The application is designed to be scalable to accommodate the evolving needs of the organization.',
+    technologies: ['Swift', 'SwiftUI', 'Python', 'Flask', 'Firebase', 'Firestore'],
+    link: 'https://github.com/SunTheCoder/LCVASwiftPortal',
+    imageUrl: '/projects/lcva-portal.png',
+  },
+  {
+    id: 'bad-kitty',
+    name: 'Bad Kitty Does Not Like Art Museums',
+    description: 'This repo is full stack application using Vanilla JavaScript, Express, AWS S3 Storage, HTML, CSS technologies.',
+    technologies: ['JavaScript', 'Express', 'AWS S3 Storage', 'HTML', 'CSS'],
+    link: 'https://github.com/SunTheCoder/Bad-Kitty-Project',
+    imageUrl: '/projects/bad-kitty.png',
+  },
+  {
+    id: 'space-case',
+    name: 'SpaceCase',
+    description: 'This repo is a containerized full stack application using Python, Flask, JavaScript, Vite, React, Redux, Web3, MetaMask, Infura, Docker, ChakraUI, CSS, HTML technologies. Making uploads on Web3 feel as natural Web2.',
+    technologies: ['Python', 'Flask', 'JavaScript', 'Vite', 'React', 'Redux', 'Web3', 'MetaMask', 'Infura', 'Docker', 'ChakraUI', 'CSS', 'HTML'],
+    link: 'https://github.com/SunTheCoder/testWeb3Project2',
+    imageUrl: '/projects/spacecase.png',
+  },
+  {
     id: 'cec-website',
     name: 'Clean Energy Connectors',
     description: 'Full-Stack application using React, JavaScript, Python, Flask, Redux, TailwindCSS, Leaflet to map ArcGis and GeoJson data in order to build a more streamlined connection to grant funding and grant information for Indigenous Nations.',
@@ -27,36 +51,12 @@ const initialProjects = [
     imageUrl: '/projects/art-recommendation.png',
   },
   {
-    id: 'space-case',
-    name: 'SpaceCase',
-    description: 'This repo is a containerized full stack application using Python, Flask, JavaScript, Vite, React, Redux, Web3, MetaMask, Infura, Docker, ChakraUI, CSS, HTML technologies. Making uploads on Web3 feel as natural Web2.',
-    technologies: ['Python', 'Flask', 'JavaScript', 'Vite', 'React', 'Redux', 'Web3', 'MetaMask', 'Infura', 'Docker', 'ChakraUI', 'CSS', 'HTML'],
-    link: 'https://github.com/SunTheCoder/testWeb3Project2',
-    imageUrl: '/projects/spacecase.png',
-  },
-  {
     id: 'redis-gallery',
     name: 'Redis Gallery',
     description: 'A gallery of images stored in Redis. Demo for creating a larger gallery management app built for speed and team collaboration',
     technologies: ["Redis", "Next.js", "React", "TailwindCSS", "Vercel"],
     link: 'https://next-vercel-kv-demo.vercel.app/',
     imageUrl: '/projects/redis-gallery.png',
-  },
-  {
-    id: 'lcva-portal',
-    name: 'LCVA Portal',
-    description: 'In development is an iOS application for Longwood University/the Longwood Center of Visual Arts. The application, utilizing Swift/SwiftUI and Python/Flask, aims to enhance education, engagement, and business analytics across the campus. The application is designed to be scalable to accommodate the evolving needs of the organization.',
-    technologies: ['Swift', 'SwiftUI', 'Python', 'Flask', 'Firebase', 'Firestore'],
-    link: 'https://github.com/SunTheCoder/LCVASwiftPortal',
-    imageUrl: '/projects/lcva-portal.png',
-  },
-  {
-    id: 'bad-kitty',
-    name: 'Bad Kitty Does Not Like Art Museums',
-    description: 'This repo is full stack application using Vanilla JavaScript, Express, AWS S3 Storage, HTML, CSS technologies.',
-    technologies: ['JavaScript', 'Express', 'AWS S3 Storage', 'HTML', 'CSS'],
-    link: 'https://github.com/SunTheCoder/Bad-Kitty-Project',
-    imageUrl: '/projects/bad-kitty.png',
   }
 ];
 // Visit /api/projects/seed in your browser to trigger the seeding. Then visit /api/projects to verify the projects are there. 
